eb39f2b2f3d88d330c327d3c765b317f2b364538
loganwe <91347950+loganwe@users.noreply.github.com>
2022-03-18 09:38:03 -0400

    commit2

--- game.js
+++ game.js
@@ -1,15 +1,22 @@
 // let ready= prompt("Are you ready?");
 let a1;
-function level1() {
-  let answer1 = prompt(
-    "leve1:What numbers none of whitch are 0 = the same thing when added or mut.?"
-  );
-  if (answer1 === "123") {
-    a1 = "cerect";
-    alert("cerect");
-  }
+let a2;
+//level1
+let answer1 = prompt(
+  "leve1:What numbers none of whitch are 0 = the same thing when added or mut.?"
+);
+if (answer1 === "123" || answer1 === "321" || answer1 === "213") {
+  a1 = "cerect";
+  alert("cerect");
 }
-
-level1();
 if (a1 === "cerect") {
+  //level2
+  let answer2 = prompt("tet");
+  if (answer2 === "") {
+    a2 = "cerect";
+    alert("cerect");
+    if (a2 === "cerect") {
+      //level3
+    }
+  }
 }
