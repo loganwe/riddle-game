98df3681b3bc49884d3b05efb248233a37f2b56a
loganwe <91347950+loganwe@users.noreply.github.com>
2022-03-18 11:29:58 -0400

    commmit3

--- game.js
+++ game.js
@@ -1,6 +1,9 @@
 // let ready= prompt("Are you ready?");
 let a1;
 let a2;
+let a3;
+let a4;
+let a5;
 //level1
 let answer1 = prompt(
   "leve1:What numbers none of whitch are 0 = the same thing when added or mut.?"
@@ -11,8 +14,8 @@ if (answer1 === "123" || answer1 === "321" || answer1 === "213") {
 }
 if (a1 === "cerect") {
   //level2
-  let answer2 = prompt("tet");
-  if (answer2 === "") {
+  let answer2 = prompt("Whitch is bigger?");
+  if (answer2 === "Whitch") {
     a2 = "cerect";
     alert("cerect");
     if (a2 === "cerect") {
