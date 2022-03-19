590709e09177474efeec565d3381f2db81b3422b
loganwe <91347950+loganwe@users.noreply.github.com>
2022-03-19 12:22:20 -0400

    made levels 4&5

--- game.js
+++ game.js
@@ -4,6 +4,11 @@ let a2;
 let a3;
 let a4;
 let a5;
+let a6;
+let a7;
+let a8;
+let a9;
+let a10;
 /*level1*/
 let answer1 = prompt(
   "leve1:What numbers none of whitch are 0 equal the same thing when added or multiplied?"
@@ -26,6 +31,22 @@ if (a1 === "cerect") {
         alert("cerect");
         if (a3 === "cerect") {
           /*level4*/
+          let answer4 = prompt("Pick a number 1-3");
+          if (answer4 === "a number 1-3") {
+            a4 = "cerect";
+            alert("cerect");
+            if (a4 === "cerect") {
+              /**level5*/
+              let answer5 = prompt("Is this this the best game ever?");
+              if (answer5 === "yes" || "Yes") {
+                a5 = "cerect";
+                alert("cerect");
+                if (a5 === "cerect") {
+                  /**level6 */
+                }
+              }
+            }
+          }
         }
       }
     }
