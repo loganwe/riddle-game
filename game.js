4f3719d2c5b381c725ee72be9c1229b2e990afd9
loganwe <91347950+loganwe@users.noreply.github.com>
2022-03-19 11:57:57 -0400

    comment changes

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
-// Checking vars
+/**  Checking vars**/
 let a1;
 let a2;
 let a3;
 let a4;
 let a5;
-//level1
+/*level1*/
 let answer1 = prompt(
   "leve1:What numbers none of whitch are 0 equal the same thing when added or multiplied?"
 );
@@ -13,19 +13,19 @@ if (answer1 === "123" || answer1 === "321" || answer1 === "213") {
   alert("cerect");
 }
 if (a1 === "cerect") {
-  //level2
+  /*level2*/
   let answer2 = prompt("Whitch is bigger?");
   if (answer2 === "Whitch") {
     a2 = "cerect";
     alert("cerect");
     if (a2 === "cerect") {
-      //level3
+      /*level3*/
       let answer3 = prompt("Do not skip this.");
       if (answer3 === "ok" || "Ok") {
         a3 = "cerect";
         alert("cerect");
         if (a3 === "cerect") {
-          //level4
+          /*level4*/
         }
       }
     }
